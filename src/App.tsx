aecd4a630356f02e7c35ab0fcb098ae51cb594f8
mitchelst <mitchel@bendingtherules.nl>
2021-06-24 03:40:34 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,43 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [authed, setAuthed] = useState(false);
+
+  return (
+    <>
+      <h1>The TypeScript Times</h1>
+      <h2>MEN WALK ON THE MOON</h2>
+      <p>Astronauts land on plain; collect rocks, plant flag.</p>
+
+      {!authed && (
+        <p>
+          please log in if you want to read more{" "}
+          <button onClick={() => setAuthed(true)}>login</button>
+        </p>
+      )}
+
+      {authed && (
+        <>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Consectetur, totam laudantium expedita hic asperiores vel eligendi
+            atque sunt odit commodi minima ipsam mollitia delectus sapiente
+            officia rem obcaecati doloribus necessitatibus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            quaerat maiores ex earum deserunt voluptatem optio reprehenderit sit
+            magnam necessitatibus ab iusto nihil vero, ea corporis, quasi cum
+            amet aut!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus consectetur quia, quibusdam perferendis illo delectus
+            ea magni hic ut velit earum necessitatibus reiciendis. Consequatur
+            cumque ab magni provident inventore sunt.
+          </p>
+        </>
+      )}
+    </>
+  );
 }
